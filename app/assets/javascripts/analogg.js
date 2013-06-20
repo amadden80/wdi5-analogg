898d2b80e8ea2dba0f4b0a53adb65724c8296d5c
Dylan O'Shea <dylan.j.oshea@gmail.com>
2013-06-20 16:23:53 -0400

    Seconds have randomly generated color

--- app/assets/javascripts/analogg.js
+++ app/assets/javascripts/analogg.js
@@ -1,8 +1,10 @@
 var second_timer;
 
 function add_second(){
+  console.log('Second added.');
   var second = $('<div>');
   second.addClass('second');
+  second.css('background-color', random_color());
   $('#seconds .time-holder').append(second);
 }
 
@@ -31,25 +33,32 @@ function shuffle_hours(){
 }
 
 function random_color(){
-
+  var red = random_rgb_int();
+  var green = random_rgb_int();
+  var blue = random_rgb_int();
+  var color = "rgb("+ red +","+ green +","+ blue +")";
+  return color;
 }
+
+function random_rgb_int(){
+  return _.random(0, 255);
+}
+
+
+
+
+
+
+
+
+
+
 
 
 //jQuery alternative to $(document).ready(function(){}); that is prettier (less verbose)
 $(function(){
 
   //Second timer
+  second_timer = setInterval(add_second, 1000);
 
 });
-
-
-
-
-
-
-
-
-
-
-
-
