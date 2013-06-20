c24ba46d03cf2583ac3fd7c5138fa918615cbe8f
Dylan O'Shea <dylan.j.oshea@gmail.com>
2013-06-20 17:11:36 -0400

    Removing unused functions and white space

--- app/assets/javascripts/analogg.js
+++ app/assets/javascripts/analogg.js
@@ -41,18 +41,6 @@ function check_time(){
   }
 }
 
-function shuffle_seconds(){
-
-}
-
-function shuffle_minutes(){
-
-}
-
-function shuffle_hours(){
-
-}
-
 function random_color(){
   var red = random_rgb_int();
   var green = random_rgb_int();
@@ -64,17 +52,6 @@ function random_color(){
 function random_rgb_int(){
   return _.random(0, 255);
 }
-
-
-
-
-
-
-
-
-
-
-
 
 //jQuery alternative to $(document).ready(function(){}); that is prettier (less verbose)
 $(function(){
