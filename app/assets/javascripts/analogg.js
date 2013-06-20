1317270be4cc58271695800a98eaed1a2b6c6912
Dylan O'Shea <dylan.j.oshea@gmail.com>
2013-06-20 15:47:11 -0400

    Skeleton of functions

--- app/assets/javascripts/analogg.js
+++ app/assets/javascripts/analogg.js
@@ -1,4 +1,53 @@
+var second_timer;
+
+function add_second(){
+
+}
+
+function add_minute(){
+
+}
+
+function add_hour(){
+
+}
+
+function check_time(){
+
+}
+
+function shuffle_seconds(){
+
+}
+
+function shuffle_minutes(){
+
+}
+
+function shuffle_hours(){
+
+}
+
+function random_color(){
+
+}
+
+
 //jQuery alternative to $(document).ready(function(){}); that is prettier (less verbose)
 $(function(){
 
+  //Second timer
+
 });
+
+
+
+
+
+
+
+
+
+
+
+
